7369d9e88a085bb71663859585b97d6b4922871c
GerritPathuis <g.pathuis@quicknet.nl>
2017-07-23 13:05:59 +0200

    Now works

---
+++ Control_Engi/Control_Engi/Form1.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Input value"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.DecimalPlaces = 1
+        Me.NumericUpDown1.Location = New System.Drawing.Point(184, 47)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {10, 0, 0, -2147483648})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(69, 20)
+        Me.NumericUpDown1.TabIndex = 1
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {2, 0, 0, 0})
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.DecimalPlaces = 3
+        Me.NumericUpDown2.Location = New System.Drawing.Point(184, 125)
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(69, 20)
+        Me.NumericUpDown2.TabIndex = 2
+        '
+        'NumericUpDown3
+        '
+        Me.NumericUpDown3.DecimalPlaces = 1
+        Me.NumericUpDown3.Location = New System.Drawing.Point(184, 73)
+        Me.NumericUpDown3.Name = "NumericUpDown3"
+        Me.NumericUpDown3.Size = New System.Drawing.Size(69, 20)
+        Me.NumericUpDown3.TabIndex = 3
+        Me.NumericUpDown3.Value = New Decimal(New Integer() {10, 0, 0, 0})
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(44, 127)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Output"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(44, 75)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Tou [sec]"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(47, 209)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(206, 40)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Integrator time step"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(47, 267)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(206, 40)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "First order time step"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(146, 159)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(107, 20)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Chart1
+        '
+        ChartArea2.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea2)
+        Legend2.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend2)
+        Me.Chart1.Location = New System.Drawing.Point(383, 24)
+        Me.Chart1.Name = "Chart1"
+        Series2.ChartArea = "ChartArea1"
+        Series2.Legend = "Legend1"
+        Series2.Name = "Series1"
+        Me.Chart1.Series.Add(Series2)
+        Me.Chart1.Size = New System.Drawing.Size(421, 350)
+        Me.Chart1.TabIndex = 9
+        Me.Chart1.Text = "Chart1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(816, 386)
+        Me.Controls.Add(Me.Chart1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.NumericUpDown3)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents NumericUpDown3 As NumericUpDown
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+End Class
---
+++ Control_Engi/Control_Engi/Form1.vb
@@ -0,0 +1,93 @@
+﻿
+Imports System.IO
+
+Public Class Form1
+
+    Public _p2 As Double        'For intergrator
+    Public _yold As Double      'For 1st order
+    Public time As Double = 0
+
+    Public Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Integrator
+        Dim τ, p1, p2, dt As Double
+
+        dt = 1                          'time step [sec]
+        p1 = NumericUpDown1.Value       'Input value
+        τ = NumericUpDown3.Value        'Time constant
+        p2 = Integrator(p1, τ, dt)
+
+        TextBox1.Text = p2.ToString("0.00")
+        NumericUpDown2.Value = p2       'Output
+        time += dt
+        Chart1.Series(0).Points.AddXY(time, p2)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        '1st order
+        Dim τ, p1, p2, dt As Double
+        dt = 1                      'time step [sec]
+        p1 = NumericUpDown1.Value
+        τ = NumericUpDown3.Value
+        p2 = First_order(p1, τ, dt)
+
+        TextBox1.Text = p2.ToString("0.000")
+        NumericUpDown2.Value = p2
+        time += dt
+        Chart1.Series(0).Points.AddXY(time, p2)
+    End Sub
+
+    Public Function First_order(kx As Double, τ As Double, dt As Double) As Double
+        'τ.(dy/dt) + y = Kx 
+        'Hieruit volgt dy= (Kx-y) * dt/τ
+        'kx = input, tou= time constant, dt= time step
+        'y is de output
+        Dim dy, y As Double 'output
+
+        dy = (kx - _yold) * dt / τ
+        y = _yold + dy
+        _yold = y
+        Return (y)
+    End Function
+
+    Public Function Integrator(x As Double, τ As Double, dt As Double) As Double
+        'teruggekoppelde integrator 
+        'Hieruit volgt dy=x/τ*dt
+        'P1 = input, tou= time constant, dt= time step
+        'return value is output value
+        Dim dp As Double
+        dp = (x / τ) * dt
+        _p2 = _p2 + dp
+        Return (_p2)
+    End Function
+
+    Private Sub Init_Chart1()
+        Dim i As Integer
+        Try
+            Chart1.Series.Clear()
+            Chart1.ChartAreas.Clear()
+            Chart1.Titles.Clear()
+            Chart1.ChartAreas.Add("ChartArea0")
+
+            For i = 0 To 1
+                Chart1.Series.Add(i.ToString)
+                Chart1.Series(i.ToString).ChartArea = "ChartArea0"
+                Chart1.Series(i.ToString).ChartType = DataVisualization.Charting.SeriesChartType.Line
+                Chart1.Series(i.ToString).BorderWidth = 3
+            Next
+
+            Chart1.Titles.Add("CONTROL ENGINEERING")
+            Chart1.Titles(0).Font = New Font("Arial", 12, System.Drawing.FontStyle.Bold)
+            Chart1.Series(0).Color = Color.Black
+            Chart1.ChartAreas("ChartArea0").AxisX.Title = "[sec]"
+            Chart1.ChartAreas("ChartArea0").AxisY.Title = "Value"
+        Catch ex As Exception
+            MessageBox.Show("Init Chart1 failed")
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Init_Chart1()
+    End Sub
+End Class
+
+
